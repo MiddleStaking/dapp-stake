df36827b0f988fe778b2f37f91fa2667f75e0e21
raphael-co <76869389+raphael-co@users.noreply.github.com>
2023-04-06 17:35:04 +0200

    responsive Tokenomics and Rewards OK

--- src/pages/Tokenomics/index.tsx
+++ src/pages/Tokenomics/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faExternalLinkAlt, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FormatAmount } from '@multiversx/sdk-dapp/UI/FormatAmount';
 import axios from 'axios';
 import { Col, Container, Row } from 'react-bootstrap';
+import { network } from 'config';
 import styles from './tokenomics.module.scss';
 
 const Tokenomics = () => {
@@ -75,8 +76,8 @@ const Tokenomics = () => {
   return (
     <div>
       {/* <div className='row'> */}
-      <div className={'col-12 col-md-10 mx-auto' + ' '}>
-        <div className={'card shadow-sm' + ' ' + styles.colorborder}>
+      <div className={'col-12 col-md-10 col-sm-10 mx-auto'}>
+        <div className={styles.colorborder}>
           <div className={'card-body p-1 mx-auto'}>
             <div
               className={
@@ -86,7 +87,10 @@ const Tokenomics = () => {
               }
             >
               <div className={'card-body text-center p-4 '}>
-                <Container className={'text-white ' + styles.cardback}>
+                <Container
+                  style={{ marginLeft: 0 }}
+                  className={'text-white ' + styles.cardback}
+                >
                   <Row
                     className={
                       'sm-12 msbulle' +
@@ -130,11 +134,49 @@ const Tokenomics = () => {
                           />
                           <Row>
                             <Col className='lead'>
-                              {item.contract_address_32}
+                              <div className={`${styles.heading}`}>
+                                <div className={styles.meta}>
+                                  <div className='d-flex align-items-center'>
+                                    <span className={styles.contract}>
+                                      {item.contract_address_32}
+                                    </span>
+                                    <a
+                                      href={`${network.explorerAddress}/accounts/${item.contract_address_32}`}
+                                      className={styles.icon}
+                                      rel='noreferrer'
+                                      target='_blank'
+                                    >
+                                      <FontAwesomeIcon
+                                        icon={faExternalLinkAlt}
+                                      />
+                                    </a>
+                                  </div>
+                                </div>
+                              </div>
                             </Col>
                           </Row>{' '}
                           <Row>
-                            <Col className='lead'>{item.wallet_address_32}</Col>
+                            <Col className='lead'>
+                              <div className={`${styles.heading}`}>
+                                <div className={styles.meta}>
+                                  <div className='d-flex align-items-center'>
+                                    <span className={styles.contract}>
+                                      {item.wallet_address_32}
+                                    </span>
+                                    <a
+                                      href={`${network.explorerAddress}/accounts/${item.wallet_address_32}`}
+                                      className={styles.icon}
+                                      rel='noreferrer'
+                                      target='_blank'
+                                    >
+                                      <FontAwesomeIcon
+                                        icon={faExternalLinkAlt}
+                                      />
+                                    </a>
+                                  </div>
+                                </div>
+                              </div>
+                            </Col>
                           </Row>
                           <hr
                             style={{
