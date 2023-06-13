68c0efd65d9cdfba196ff2105b53f515e648d50f
Kevin Lallement <lallement.k@gmail.com>
2023-06-13 10:52:22 +0200

    remove jakarta

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ export const App = () => {
         </svg>
         <Router>
           <DappProvider
-            environment={EnvironmentsEnum.devnet}
+            environment={EnvironmentsEnum.mainnet}
             customNetworkConfig={{
               name: 'customConfig',
               apiTimeout,
