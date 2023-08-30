4e7c7d2e638a0ef5d644eb827338978183b66667
Kevin Lallement <lallement.k@gmail.com>
2023-08-30 11:53:11 +0200

    ready to branch

--- src/pages/Earn/components/CardPool/component/MyStakeSection.tsx
+++ src/pages/Earn/components/CardPool/component/MyStakeSection.tsx
@@ -76,8 +76,8 @@ const MyStakeSection: FC<MyStakeSectionProps> = ({
     flexShrink: 0,
     position: 'relative',
     overflow: 'hidden',
-    width: '100%',
-    height: '202px'
+    width: '100%'
+    // height: '202px'
   };
 
   const MyStackedStyle: CSSProperties = {
