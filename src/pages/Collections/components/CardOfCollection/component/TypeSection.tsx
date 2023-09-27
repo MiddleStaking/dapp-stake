d8e6b0117967acb703f19fb76d2da3ecc8904f61
Kevin Lallement <lallement.k@gmail.com>
2023-09-28 00:55:14 +0200

    button plz

--- src/pages/Collections/components/CardOfCollection/component/TypeSection.tsx
+++ src/pages/Collections/components/CardOfCollection/component/TypeSection.tsx
@@ -10,6 +10,7 @@ import SwowHideDetails from './SwowHideDetails';
 import ReactPlayer from 'react-player';
 import HexagoneNFT from '../../hexagoneNFT';
 import { PoolAddCollection } from '../../Modal/AddCollection/PoolAddCollection';
+import { Button } from 'components/Design';
 
 interface TypeSectionProps {
   height: string;
@@ -302,6 +303,7 @@ const TypeSection: FC<TypeSectionProps> = ({
               <div style={rate}>10000</div>
               <div style={rate}>10%</div>
             </div>
+
             <div
               onClick={() =>
                 (window.location.href = `/collections/${collectionIdentifier}`)
@@ -309,7 +311,7 @@ const TypeSection: FC<TypeSectionProps> = ({
               className='butLine bouton-visiter'
               data-testid='loginBtn'
             >
-              redirect
+              redirect sqd
               {/* <div className='enteteCard'> */}
               {/* </div> 
               <div>icône blé xxxxxxx</div>
@@ -373,6 +375,19 @@ const TypeSection: FC<TypeSectionProps> = ({
 
         {/* {!showDetails ? ( */}
       </div>
+      <div style={{ margin: 'auto' }}>
+        <Button
+          borderRadius={40}
+          buttonHeight='31px'
+          // buttonWidth='240px'
+          textColor='#ffffff'
+          background={'#000000'}
+          onClick={() =>
+            (window.location.href = `/collections/${collectionIdentifier}`)
+          }
+          text={'Rewards'}
+        />
+      </div>
       <div style={detailsStyle} onClick={handleChange}>
         <div style={{ fontSize: '10px' }}>
           {!showDetails ? 'Show details' : 'Hide details'}
@@ -407,6 +422,7 @@ const TypeSection: FC<TypeSectionProps> = ({
           </svg>
         )}
       </div>
+
       <div style={{ display: !showDetails ? 'none' : 'block', width: '100%' }}>
         {/* <SwowHideDetails
           rewarded_esdt_info={rewarded_esdt_info}
